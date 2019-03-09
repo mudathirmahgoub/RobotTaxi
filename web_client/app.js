ddc853384812594d64d4738781e5f526854c88b5
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-09 15:06:45 -0600

    changes

--- web_client/app.js
+++ web_client/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var data = [{ date: "10/25/2018", value: 1 },
-    { date: "10/26/2018", value: 3 },
-    { date: "10/27/2018", value: 0 },
-    { date: "10/28/2018", value: 0 },
-    { date: "10/29/2018", value: 5 },
-    { date: "10/30/2018", value: 8 },
-    { date: "10/31/2018", value: 7 },
-    { date: "11/01/2018", value: 11 },
-    { date: "11/02/2018", value: 23 },
-    { date: "11/03/2018", value: 13 },
-    { date: "11/04/2018", value: 15 },
-    { date: "11/05/2018", value: 37 }
-   ];
+{ date: "10/26/2018", value: 3 },
+{ date: "10/27/2018", value: 0 },
+{ date: "10/28/2018", value: 0 },
+{ date: "10/29/2018", value: 5 },
+{ date: "10/30/2018", value: 8 },
+{ date: "10/31/2018", value: 7 },
+{ date: "11/01/2018", value: 11 },
+{ date: "11/02/2018", value: 23 },
+{ date: "11/03/2018", value: 13 },
+{ date: "11/04/2018", value: 15 },
+{ date: "11/05/2018", value: 37 }
+];
 
 var margin = 50;
 
@@ -27,8 +27,8 @@ var dataGroup = d3.select("body").append("svg")
 
 var line = d3.line()
     .x(d => x(d.date))
-.y(d => y(d.value))
-;
+    .y(d => y(d.value))
+    ;
 
 var parseTime = d3.timeParse("%m/%d/%Y");
 
@@ -39,12 +39,12 @@ data.forEach(function (d) {
 var x = d3.scaleTime()
     .domain(d3.extent(data, function (d) { return d.date; }))
     .range([0, width])
-;
+    ;
 
 var y = d3.scaleLinear()
     .domain(d3.extent(data, function (d) { return d.value }))
     .range([height, 0])
-;
+    ;
 
 dataGroup.append("path")
     .data([data])
