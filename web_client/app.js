1bbb90cbfac8dc1d5b35c0af658dc9c3b7f08b9e
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-18 22:31:22 -0500

    cleaned data

--- web_client/app.js
+++ web_client/app.js
@@ -4,11 +4,8 @@
 
 function gridData() {
     var data = new Array();
-    var xPosition = 1; //starting xPosition and yPosition at 1 so the stroke will show when we make the grid below
-    var yPosition = 1;
-    var width = 50;
-    var height = 50;
-
+    var x = 0; //starting x and y at 1 so the stroke will show when we make the grid below
+    var y = 0;
     //add this to the gridData function
     var click = 0;
 
@@ -19,19 +16,19 @@ function gridData() {
         // iterate for cells/columns inside rows
         for (var column = 0; column < 10; column++) {
             data[row].push({
-                x: xPosition,
-                y: yPosition,
-                width: width,
-                height: height,
+                x: x,
+                y: y,
+                width: 50,
+                height: 50,
                 click: click
             })
-            // increment the x position. I.e. move it over by 50 (width variable)
-            xPosition += width;
+            // increment the x position
+            x += 1;
         }
         // reset the x position after a row is complete
-        xPosition = 1;
-        // increment the y position for the next row. Move it down 50 (height variable)
-        yPosition += height;
+        x = 0;
+        // increment the y position for the next row
+        y += 1;
     }
     return data;
 }
@@ -40,8 +37,8 @@ var gridData = gridData();
 
 var grid = d3.select('#grid')
 .append('svg')
-.attr('width', '510px')
-.attr('height', '510px');
+.attr('width', '500px')
+.attr('height', '500px');
 
 
 var row = grid.selectAll(".row")
@@ -54,13 +51,14 @@ var column = row.selectAll(".square")
     .data(function(d) { return d; })
     .enter().append("rect")
     .attr("class","square")
-    .attr("x", function(d) { return d.x; })
-    .attr("y", function(d) { return d.y; })
-    .attr("width", function(d) { return d.width; })
+    .attr("x", function(d) { return d.x * 50; })
+    .attr("y", function(d) { return d.y * 50; })
+    .attr("width", function(d) { return d.width ; })
     .attr("height", function(d) { return d.height; })
     .style("fill", "#fff")
     .style("stroke", "#222")
     .on('click', function(d){
+        console.log(d);
         d.click ++;
         if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
         if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
