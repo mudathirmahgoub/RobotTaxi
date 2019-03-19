5cfc913d06c8f68fdb52054cb2f57b8ed668aee2
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-19 12:32:52 -0500

    map draft

--- web_client/app.js
+++ web_client/app.js
@@ -2,16 +2,16 @@
 
 //based on http://www.cagrimmett.com/til/2016/08/17/d3-lets-make-a-grid.html
 
-function gridData() {
-    var data = new Array();
+function getGridData() {
+    var data = [];
     var x = 0; //starting x and y at 1 so the stroke will show when we make the grid below
     var y = 0;
-    //add this to the gridData function
+    //add this to the getGridData function
     var click = 0;
 
     // iterate for rows
     for (var row = 0; row < 10; row++) {
-        data.push( new Array() );
+        data.push( [] );
 
         // iterate for cells/columns inside rows
         for (var column = 0; column < 10; column++) {
@@ -21,7 +21,7 @@ function gridData() {
                 width: 50,
                 height: 50,
                 click: click
-            })
+            });
             // increment the x position
             x += 1;
         }
@@ -33,7 +33,7 @@ function gridData() {
     return data;
 }
 
-var gridData = gridData();
+var gridData = getGridData();
 
 var grid = d3.select('#grid')
 .append('svg')
@@ -70,20 +70,8 @@ var column = row.selectAll(".square")
     .on('click', function(d){
         console.log(d);
         d.click ++;
-        if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
-        if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
-        if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
-        if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
+        if ((d.click)%4 === 0 ) { d3.select(this).style("fill","#fff"); }
+        if ((d.click)%4 === 1 ) { d3.select(this).style("fill","#2C93E8"); }
+        if ((d.click)%4 === 2 ) { d3.select(this).style("fill","#F56C4E"); }
+        if ((d.click)%4 === 3 ) { d3.select(this).style("fill","#838690"); }
     });
-
-var grid = d3.select('#grid')
-    .append('svg')
-    .attr('width', '100%')
-    .attr('height', '100%');
-
-
-var row = grid.selectAll(".row")
-    .data(map)
-    .enter().append('g')
-    .attr('class', 'row');
-
