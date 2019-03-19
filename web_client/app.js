cbb8608afc390ccfdaa4a268e511e0d4c5207a58
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-18 22:21:21 -0500

    blog

--- web_client/app.js
+++ web_client/app.js
@@ -1,71 +1,69 @@
 'use strict';
 
-var data = [{ date: "10/25/2018", value: 1 },
-{ date: "10/26/2018", value: 3 },
-{ date: "10/27/2018", value: 0 },
-{ date: "10/28/2018", value: 0 },
-{ date: "10/29/2018", value: 5 },
-{ date: "10/30/2018", value: 8 },
-{ date: "10/31/2018", value: 7 },
-{ date: "11/01/2018", value: 11 },
-{ date: "11/02/2018", value: 23 },
-{ date: "11/03/2018", value: 13 },
-{ date: "11/04/2018", value: 15 },
-{ date: "11/05/2018", value: 37 }
-];
+//based on http://www.cagrimmett.com/til/2016/08/17/d3-lets-make-a-grid.html
 
-var margin = 50;
+function gridData() {
+    var data = new Array();
+    var xPosition = 1; //starting xPosition and yPosition at 1 so the stroke will show when we make the grid below
+    var yPosition = 1;
+    var width = 50;
+    var height = 50;
 
-var width = 1024;
-var height = 768;
+    //add this to the gridData function
+    var click = 0;
 
-var dataGroup = d3.select("body").append("svg")
-    .attr("width", width + margin)
-    .attr("height", height + 2 * margin)
-    .append("g")
-    .attr("transform", "translate(" + margin + ", " + margin + ")");
+    // iterate for rows
+    for (var row = 0; row < 10; row++) {
+        data.push( new Array() );
 
-var line = d3.line()
-    .x(d => x(d.date))
-    .y(d => y(d.value))
-    ;
+        // iterate for cells/columns inside rows
+        for (var column = 0; column < 10; column++) {
+            data[row].push({
+                x: xPosition,
+                y: yPosition,
+                width: width,
+                height: height,
+                click: click
+            })
+            // increment the x position. I.e. move it over by 50 (width variable)
+            xPosition += width;
+        }
+        // reset the x position after a row is complete
+        xPosition = 1;
+        // increment the y position for the next row. Move it down 50 (height variable)
+        yPosition += height;
+    }
+    return data;
+}
 
-var parseTime = d3.timeParse("%m/%d/%Y");
+var gridData = gridData();
 
-data.forEach(function (d) {
-    d.date = parseTime(d.date);
-});
+var grid = d3.select('#grid')
+.append('svg')
+.attr('width', '510px')
+.attr('height', '510px');
 
-var x = d3.scaleTime()
-    .domain(d3.extent(data, function (d) { return d.date; }))
-    .range([0, width])
-    ;
 
-var y = d3.scaleLinear()
-    .domain(d3.extent(data, function (d) { return d.value }))
-    .range([height, 0])
-    ;
+var row = grid.selectAll(".row")
+.data(gridData)
+.enter().append('g')
+.attr('class', 'row');
 
-dataGroup.append("path")
-    .data([data])
-    .attr("fill", "none")
-    .attr("stroke", "red")
-    .attr("d", line)
 
-var xAxisGroup = dataGroup
-    .append("g")
-    .attr("class", "xAxisGroup")
-    .attr("transform", "translate(0," + height + ")")
-
-var xAxis = d3.axisBottom(x)
-    .tickFormat(d3.timeFormat("%Y-%m-%d"));
-
-xAxis(xAxisGroup);
-
-var yAxisGroup = dataGroup
-    .append("g")
-    .attr("class", "yAxisGroup")
-
-var yAxis = d3.axisLeft(y);
-
-yAxis(yAxisGroup);
+var column = row.selectAll(".square")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+    .attr("class","square")
+    .attr("x", function(d) { return d.x; })
+    .attr("y", function(d) { return d.y; })
+    .attr("width", function(d) { return d.width; })
+    .attr("height", function(d) { return d.height; })
+    .style("fill", "#fff")
+    .style("stroke", "#222")
+    .on('click', function(d){
+        d.click ++;
+        if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
+        if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
+        if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
+        if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
+    });
