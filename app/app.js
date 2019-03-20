4f3793aace74ffaee452a6d1f33e638cda1300cf
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-20 12:50:49 -0500

    robot location

--- app/app.js
+++ app/app.js
@@ -7,10 +7,23 @@ var svg = d3.select('svg')
     .attr('height', '100%')
     .style('background-color', '#707477');
 
+var cellLength = 50;
+
 d3.json('/map').then(function (mapData){
     console.log(mapData);
     displayMap(mapData);
+    displayGrid();
+    displayRobots();
 });
+
+// dynamic data
+
+var robotsData = [
+    {"robot_id": 1, "x": 125, "y": 120, "angle_z_degrees": 0, "update_time": ""},
+    {"robot_id": 2, "x": 225, "y": 120, "angle_z_degrees": 0, "update_time": ""},
+    {"robot_id": 3, "x": 325, "y": 220, "angle_z_degrees": 0, "update_time": ""},
+    {"robot_id": 4, "x": 425, "y": 220, "angle_z_degrees": 0, "update_time": ""}
+];
 
 function displayMap(mapData){
 
@@ -58,59 +71,96 @@ function displayMap(mapData){
         .attr('height', function(data) { return cellLength; });
 }
 
-function getGridData() {
-    var data = [];
-    var x = 0; //starting x and y at 1 so the stroke will show when we make the svg below
-    var y = 0;
-    //add this to the getGridData function
-    var click = 0;
+function displayGrid() {
 
-    // iterate for rows
-    for (var row = 0; row < 9; row++) {
-        data.push( [] );
+    function getGridData() {
+        var data = [];
+        var x = 0; //starting x and y at 1 so the stroke will show when we make the svg below
+        var y = 0;
+        //add this to the getGridData function
+        var click = 0;
 
-        // iterate for cells/columns inside rows
-        for (var column = 0; column < 9; column++) {
-            data[row].push({
-                x: x,
-                y: y,
-                click: click
-            });
-            // increment the x position
-            x += 1;
+        // iterate for rows
+        for (var row = 0; row < 9; row++) {
+            data.push([]);
+
+            // iterate for cells/columns inside rows
+            for (var column = 0; column < 9; column++) {
+                data[row].push({
+                    x: x,
+                    y: y,
+                    click: click
+                });
+                // increment the x position
+                x += 1;
+            }
+            // reset the x position after a row is complete
+            x = 0;
+            // increment the y position for the next row
+            y += 1;
         }
-        // reset the x position after a row is complete
-        x = 0;
-        // increment the y position for the next row
-        y += 1;
+        return data;
     }
-    return data;
+
+    var gridData = getGridData();
+
+    var row = svg.selectAll('.row')
+        .data(gridData)
+        .enter().append('g')
+        .attr('class', 'row');
+
+    var column = row.selectAll('.square')
+        .data(function (d) {
+            return d;
+        })
+        .enter().append('rect')
+        .attr('class', 'square')
+        .attr('x', function (d) {
+            return d.x * cellLength;
+        })
+        .attr('y', function (d) {
+            return d.y * cellLength;
+        })
+        .attr('width', function (d) {
+            return cellLength;
+        })
+        .attr('height', function (d) {
+            return cellLength;
+        })
+        .style('fill', 'rgba(255, 255, 255, 0)')
+        .style('stroke', 'rgba(cellLength, cellLength, cellLength, 5)')
+        .on('click', function (d) {
+            console.log(d);
+            d.click++;
+            if ((d.click) % 4 === 0) {
+                d3.select(this).style('fill', '#fff');
+            }
+            if ((d.click) % 4 === 1) {
+                d3.select(this).style('fill', '#2C93E8');
+            }
+            if ((d.click) % 4 === 2) {
+                d3.select(this).style('fill', '#F56C4E');
+            }
+            if ((d.click) % 4 === 3) {
+                d3.select(this).style('fill', '#838690');
+            }
+        });
 }
 
-var gridData = getGridData();
+function displayRobots(){
+    var robotsGroup = svg.append('g');
+    robotsGroup
+        .selectAll('.robotsData')
+        .data(robotsData)
+        .enter()
+        .append('svg:image')
+        .attr('class', 'robot')
+        .attr('xlink:href', function(data){
+           return "images/cozmoRight.png";
+        })
+        .attr('x', function(data) { return data.x; })
+        .attr('y', function(data) { return data.y; })
+        .attr('width', function(data) { return cellLength / 3 ; })
+        .attr('height', function(data) { return cellLength / 3; });
+}
 
-var cellLength = 50;
-
-var row = svg.selectAll('.row')
-    .data(gridData)
-    .enter().append('g')
-    .attr('class', 'row');
-
-var column = row.selectAll('.square')
-    .data(function(d) { return d; })
-    .enter().append('rect')
-    .attr('class','square')
-    .attr('x', function(d) { return d.x * cellLength; })
-    .attr('y', function(d) { return d.y * cellLength; })
-    .attr('width', function(d) { return cellLength; })
-    .attr('height', function(d) { return cellLength; })
-    .style('fill', 'rgba(255, 255, 255, 0)')
-    .style('stroke', 'rgba(cellLength, cellLength, cellLength, 5)')
-    .on('click', function(d){
-        console.log(d);
-        d.click ++;
-        if ((d.click)%4 === 0 ) { d3.select(this).style('fill','#fff'); }
-        if ((d.click)%4 === 1 ) { d3.select(this).style('fill','#2C93E8'); }
-        if ((d.click)%4 === 2 ) { d3.select(this).style('fill','#F56C4E'); }
-        if ((d.click)%4 === 3 ) { d3.select(this).style('fill','#838690'); }
-    });
