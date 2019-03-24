2b48567f72b831139664fd554187db94ff17ec5f
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-03-24 04:40:54 -0500

    refactoring

--- cozmo_client.py
+++ cozmo_client.py
@@ -1,48 +1,32 @@
-from shared import *
+from shared_client import *
 import cozmo
 from cozmo.util import *
-import time
-import requests
-import random
-
-api_url = 'http://192.168.0.12:7000/{0}'
-
-id_response = requests.get(api_url.format('id'))
-robot_id = id_response.json()['id']
-robot_type = 'cozmo'
-
-map_response = requests.get(api_url.format('map'))
-world_map = map_response.json()
-cell_length = world_map['cellLengthMillimeters']
-start_row, start_column = world_map['startRow'], world_map['startColumn']
 
 
-def get_map_cell(x, y):
-    row = (x + start_row * cell_length) // cell_length
-    column = (y + start_column * cell_length) // cell_length
-    cell = list(filter(lambda c: c['row'] == row and c['column'] == column, world_map['cells']))
-    return cell
+def get_coordinates(pose: Pose):
+    return pose.position.y, pose.position.x
 
 
-current_cell = get_map_cell(500, 500)
+class CozmoClient(RobotClient):
+    def __init__(self, robot: cozmo.robot.Robot):
+        x = start_row * cell_length
+        y = start_column * cell_length
+        angle_z_degrees = 0  # facing down
+        self.robot = robot
+        self.current_action: cozmo.Action = None
+        RobotClient.__init__(self, 'cozmo', x, y, angle_z_degrees)
 
-
-def post_status(x, y, angle_z_degrees=0):
-    robot_state = RobotState(robot_id, robot_type, x, y, angle_z_degrees)
-    json_data = RobotEncoder().encode(robot_state)
-    requests.post(api_url.format('robot_status/{0}').format(robot_id), data=json_data,
-                  headers={'Content-type': 'application/json'})
+    def move_randomly(self):
+        if not self.current_action or self.current_action.is_completed:
+            self.current_action = self.robot.drive_straight(distance=distance_inches(20),
+                                                            speed=speed_mmps(50), should_play_anim=False)
+        pose = self.robot.pose
+        self.x, self.y = get_coordinates(pose)
 
 
 def cozmo_program(robot: cozmo.robot.Robot):
-    map_x, map_y = 0, 0
-    robot.drive_straight(distance=distance_inches(20), speed=speed_mmps(50), should_play_anim=False)
-    while True:
-        pose = robot.pose
-        x, y = pose.position.x, pose.position.y
-        print(pose)
-        post_status(y, x)
-        time.sleep(.1)
+    cozmo_client = CozmoClient(robot)
+    loop(cozmo_client)
 
 
 cozmo.run_program(cozmo_program, use_viewer=True)
