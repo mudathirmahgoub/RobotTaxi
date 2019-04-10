4589c8593fad14be83b2b02e99d509260be90970
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-10 13:50:32 -0500

    matlab

--- onnx.py
+++ onnx.py
@@ -1,9 +1,10 @@
 import matlab.engine
 names = matlab.engine.find_matlab()
 print(names)
+# matlab.engine.shareEngine('DeepLearning')
 matlab_engine = matlab.engine.connect_matlab('DeepLearning')
 for x in range(6):
-    matlab_engine.classifyImage(nargout=0)
+    matlab_engine.classify_image(nargout=0)
     image_class = matlab_engine.eval('class')
     print(image_class)
 
