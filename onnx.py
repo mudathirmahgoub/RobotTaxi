9fd3bed1a1a1ec88a3ec5a5e1edb0917ebfb4319
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-10 14:09:14 -0500

    matlab

--- onnx.py
+++ onnx.py
@@ -1,10 +1,29 @@
+import cozmo
+import time
 import matlab.engine
 names = matlab.engine.find_matlab()
 print(names)
 # matlab.engine.shareEngine('DeepLearning')
 matlab_engine = matlab.engine.connect_matlab('DeepLearning')
-for x in range(6):
-    matlab_engine.classify_image(nargout=0)
-    image_class = matlab_engine.eval('class')
-    print(image_class)
+camera_on = True
 
+
+def on_new_camera_image(evt, **kwargs):
+    global camera_on
+    if camera_on:
+        image = kwargs['image'].raw_image
+        image.save(f"data/current_image.jpeg", "JPEG")
+        matlab_engine.classify_image(nargout=0)
+        image_class = matlab_engine.eval('class')
+        print(image_class)
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
+    while True:
+        camera_on = True
+        time.sleep(1)
+        camera_on = False
+
+
+cozmo.run_program(cozmo_program, use_viewer=True)
