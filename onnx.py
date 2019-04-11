4abca80421a7e0ca9e28f5d51f706bbc02a6dc55
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-10 19:23:26 -0500

    progress

--- onnx.py
+++ onnx.py
@@ -1,7 +1,8 @@
 import cozmo
 import time
 import matlab.engine
-
+from threading import *
+from shared_client import *
 names = matlab.engine.find_matlab()
 print(names)
 # matlab.engine.shareEngine('DeepLearning')
@@ -12,6 +13,15 @@ big_turn = 50
 small_turn = 20
 image_class = None
 processing = False
+
+
+def post_status(robot: cozmo.robot.Robot):
+    x, y = robot.pose.position.x, robot.pose.position.y
+    robot_state = RobotState(1, 'cozmo', x, y, 0)
+    json_data = RobotEncoder().encode(robot_state)
+    requests.post(api_url.format('robot_status/{0}').format(1), data=json_data,
+                  headers={'Content-type': 'application/json'})
+    Timer(0.1, post_status, kwargs={robot: robot}).start()
 
 
 def on_new_camera_image(evt, **kwargs):
@@ -31,14 +41,15 @@ wheels_speeds = {
     'straight': (left_speed, right_speed),
     'corner_left': (small_turn, right_speed),
     'corner_right': (left_speed, small_turn),
-    'turn_left': (left_speed - small_turn, right_speed),
-    'turn_right': (left_speed, right_speed - small_turn)
+    'turn_left': (left_speed - small_turn, right_speed + small_turn / 2),
+    'turn_right': (left_speed + small_turn / 2, right_speed - small_turn)
 }
 
 
 def cozmo_program(robot: cozmo.robot.Robot):
     global image_class, processing
     robot.camera.image_stream_enabled = True
+    Timer(0.1, post_status, kwargs={robot: robot}).start()
     # robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
     # global_robot.drive_wheels(left_speed, right_speed)
     while True:
