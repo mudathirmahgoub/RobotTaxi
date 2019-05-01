bf608918678132552ef3551e0ac417baa8eb73e5
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-05-01 06:05:34 -0500

    progress

--- mock_client.py
+++ mock_client.py
@@ -1,32 +1,56 @@
+from threading import *
 from shared_client import *
 
 
-class MockClient(RobotClient):
-    def __init__(self):
-        x = start_row * cell_length
-        y = start_column * cell_length
-        rotation = 0  # facing down
-        RobotClient.__init__(self, 'mock', x, y, rotation)
+left_speed, right_speed = 50, 50
+big_turn = 50
+small_turn = 5
+turn = 20
 
-    def move_randomly(self):
-        random_neighbor = RobotClient.get_random_neighbor(self)
-        # down
-        if self.rotation == 0:
-            self.x = random_neighbor['row'] * cell_length + cell_length / 5
-            self.y = random_neighbor['column'] * cell_length + cell_length / 5
-        # up
-        if self.rotation == 180:
-            self.x = random_neighbor['row'] * cell_length + 2.5 * cell_length / 5
-            self.y = random_neighbor['column'] * cell_length + 2.5 * cell_length / 5
-        # right
-        if self.rotation == 90:
-            self.x = random_neighbor['row'] * cell_length + 2.5 * cell_length / 5
-            self.y = random_neighbor['column'] * cell_length + cell_length / 5
-        # left
-        if self.rotation == -90:
-            self.x = random_neighbor['row'] * cell_length + cell_length / 5
-            self.y = random_neighbor['column'] * cell_length + 2.5 * cell_length / 5
+id_response = requests.get(api_url.format('id'))
+robot_id = id_response.json()['id']
+
+x, y, rotation = 0, 0, 0
 
 
-mock_client = MockClient()
-loop(mock_client)
+def post_status():
+    global x, y, rotation
+    print(x)
+    robot_state = RobotState(robot_id, 'cozmo', x, y, rotation)
+    json_data = RobotEncoder().encode(robot_state)
+    print(json_data)
+    requests.post(api_url.format('robot_status/{0}').format(robot_id), data=json_data,
+                  headers={'Content-type': 'application/json'})
+    Timer(0.1, post_status).start()
+
+
+def post_image(x, y):
+    row, column = x // cell_length, y // cell_length
+    return 'straight'
+
+
+wheels_speeds = {
+    'straight': (left_speed, right_speed),
+    'cross': (left_speed, right_speed),
+    'corner-left': (turn + small_turn, right_speed),
+    'corner-right': (left_speed, turn),
+    'turn-left': (left_speed - small_turn, right_speed + small_turn),
+    'turn-right': (left_speed + small_turn, right_speed - small_turn),
+    'stop': (0, 0),
+    'turn-left-left': (turn, right_speed),
+    'turn-right-right': (left_speed, turn),
+}
+
+
+def program():
+    global x, y, rotation
+    Timer(0.1, post_status).start()
+    while True:
+        image_class = post_image(x, y)
+        (left, right) = wheels_speeds.get(image_class, (0, 0))
+        x += 20
+        time.sleep(refresh_rate_milliseconds / 1000)  # convert to seconds
+
+
+if __name__ == '__main__':
+    program()
